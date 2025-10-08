6b8fb025dbd259627541a604bd31388a5aed8fc0
Buildwith18 <buildwith.18@gmail.com>
2025-10-08 23:22:02 +0530

    test commit 1.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  server: {
+    port: 5179, // 👈 change this to any free port (e.g., 5174)
   },
 });
+
